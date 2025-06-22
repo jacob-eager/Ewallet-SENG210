63920da85c6131851fffa95a0d0af3df94d1314c
Justin-Ashbau <ashbauj1@ferris.edu>
2025-06-22 18:03:49 -0400

    Create ExpenserImp.java
    Added skeleton of ExpenserImp.java to implement Expenser.java

---
+++ src/ExpenserImp.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ExpenserImp implements Expenser {
+    public User userAtHand;
+
+    public ExpenserImp(User user) {
+        this.userAtHand = user;
+    }
+
+    public void addExpense(Expense Ex) {
+
+    }
+
+    public void addMonthlyIncome(Wage W) {
+
+    }
+
+    public void PrintFullreport() {
+
+    }
+
+    public void PrintExpensereport() {
+
+    }
+
+    public void PrintIncomereport() {
+
+    }
+
+    public void PrintIncomereportbyType() {
+
+    }
+
+    public void PrintExpensebyType() {
+
+    }
+
+    public void exportReport(String reportTitle) {
+      
+    }
+
+    public Currency convertForeignCurrency(Currency C, double amount) {
+        return C; //Placeholder return statement
+    }
+
+    public boolean loadExpenseFile(String filePath) {
+        return False; //Placeholder return statement
+    }
+
+    public boolean loadIncomeFile(String filePath) {
+        return False; //Placeholder return statement
+    }
+
+    public int whenCanIBuy(String itemname, double price) {
+        return -1; //Placeholder return statement
+    }
+
+    public void updateMonthlySavings() {
+
+    }
+}
