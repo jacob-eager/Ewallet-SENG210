2285400a1651a9c766b48862e5bcec504f79a47c
Justin-Ashbau <ashbauj1@ferris.edu>
2025-06-24 14:22:52 -0400

    Update ExpenseCalculator.java
    Implemented the print by type methods

--- src/ExpenseCalculator.java
+++ src/ExpenseCalculator.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
 public class ExpenseCalculator implements Expenser {
     public User userAtHand;
@@ -28,11 +30,42 @@ public class ExpenseCalculator implements Expenser {
     }
 
     public void PrintIncomereportbyType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter income type: ");
+        String type = scanner.nextLine();
 
+        double total = 0;
+        System.out.println("Type: " + type);
+        
+        for (int i = 0; i < userAtHand.Income.size(); i++) {
+            if (userAtHand.Income.get(i).source.equalsIgnoreCase(type)) {
+                System.out.println("Amount: $" + userAtHand.Income.get(i).amount + " in " + userAtHand.Income.get(i).Month);
+                total += userAtHand.Income.get(i).amount;
+            }
+        }
+
+        System.out.println("Total income for " + type + ": $" + total + " over " + userAtHand.Income.size() + " months");
     }
 
     public void PrintExpensebyType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter expense type: ");
+        String type = scanner.nextLine();
+        
+        double total = 0;
+        System.out.println("Type: " + type);
+        
+        for (int i = 0; i < userAtHand.Spending.size(); i++) {
+            if (userAtHand.Spending.get(i).source.equalsIgnoreCase(type)) {
+                String freq = "";
+                if (userAtHand.Spending.get(i).yearlyfrequency == 1) {freq = "1 time a year";}
+                else {freq = userAtHand.Spending.get(i).yearlyfrequency + " times a year";}
+                System.out.println("Amount: $" + userAtHand.Spending.get(i).amount + ", " + freq);
+                total += userAtHand.Spending.get(i).amount * userAtHand.Spending.get(i).yearlyfrequency / 12.0;
+            }
+        }
 
+        System.out.println("Total cost for " + type + ": $" + total + " every month");
     }
 
     public void exportReport(String reportTitle) {
