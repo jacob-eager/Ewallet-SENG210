92eba3affb76dc992f737baac4f3789f7488348f
Jacob Eager <jakeeager.04@gmail.com>
2025-08-05 10:39:01 -0400

    Update EWalletApp.java

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -16,6 +16,9 @@ public class EWalletApp {
     }
 
 	public static void main(String[] args) {
+		DatabaseAccess.getURL(); 
+		DatabaseAccess.createConnection(); 
+		displayLoginScreen();
 		showReportGUI();
 	}
 	
@@ -25,14 +28,8 @@ public class EWalletApp {
 		allData = new ArrayList<User>();
 		allData.add(createTestUser());
 		currUserIndex = 0;
-		
 		new ReportFrame(allData.get(currUserIndex));
 	}
-	
-	
-	/*static void featureDemoDD() {
-		User testUser = new User("testUser", "password123");
-		ExpenseCalculator calculator = new ExpenseCalculator(testUser);*/
 
 
     // === New Login Screen Method ===
@@ -82,26 +79,51 @@ public class EWalletApp {
         frame.setVisible(true);
     }
 
-   static void featureDemoDD() {
-        User testUser = new User("testUser", "password123");
-        ExpenseCalculator calculator = new ExpenseCalculator(testUser);
+    static void featureDemoDD() {
+		User testUser = new User("testUser", "password123");
+		ExpenseCalculator calculator = new ExpenseCalculator(testUser);
+		
+		System.out.println("__Full report (and add expense/monthly income) demo__");
+		
+		calculator.addExpense(new Expense("Groceries", 500, 12));
+		calculator.addMonthlyIncome(new Wage("Salary", 3000, "January"));
+		calculator.printFullReport();
+		
+		System.out.println("__Update montly savings demo__");
 
-        System.out.println("__Full report (and add expense/monthly income) demo__");
+		System.out.println("Monthly Savings Before: " + testUser.getMonthlySavings(calculator));
+		calculator.addExpense(new Expense("Rent", 50, 24));
+	    calculator.addMonthlyIncome(new Wage("Sold old car", 4000, "February"));
+	    System.out.println("Monthly Savings After: " + testUser.getMonthlySavings(calculator));
+		
+	    System.out.println("__Print income/expense report demo__");
 
-        calculator.addExpense(new Expense("Groceries", 500, 12));
-        calculator.addMonthlyIncome(new Wage("Salary", 3000, "January"));
-        calculator.printFullReport();
+		calculator.printExpenseReport();
+		calculator.printIncomeReport();
 
-        System.out.println("__Update montly savings demo__");
+		System.out.println("__Export report and \"When Can I Buy Demo\"__");
 
-        System.out.println("Monthly Savings Before: " + testUser.getMonthlySavings(calculator));
-        calculator.addExpense(new Expense("Rent", 50, 24));
-        calculator.addMonthlyIncome(new Wage("Sold old car", 4000, "February"));
-        System.out.println("Monthly Savings After: " + testUser.getMonthlySavings(calculator));
-
-        System.out.println("__Print income/expense report demo__");
-
-    
+		calculator.exportReport("income");
+		System.out.println("Test Item: Nintendo Switch 2, $500");
+		calculator.whenCanIBuy("Nintendo Switch 2", 500);
+	    
+	}
+	static void featureDemoJA() {
+		User testUser = new User("testUser", "password123");
+		ExpenseCalculator calculator = new ExpenseCalculator(testUser);
+		
+		System.out.println("__Load expense file and load income file demo__");
+		
+		calculator.loadExpenseFile("expenses.csv");
+		calculator.loadIncomeFile("incomes.csv");
+		
+		System.out.println("__Print expense and income by type demo__");
+		
+		calculator.printExpenseByType();
+		calculator.printIncomeReportByType();
+		
+		System.out.println("__Convert foreign currency demo__");
+		
 		Currency yuan = new Currency();
 		yuan.rate = 0.14;
 		yuan.name = "CNY";
